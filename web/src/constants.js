35252ed345af8eee23022dde13977e52356f8455
Lyman Shen <lyman.shen@gmail.com>
2020-09-13 10:51:02 -0700

    Use HTTPS for Location API

--- web/src/constants.js
+++ web/src/constants.js
@@ -3,7 +3,7 @@
 export const API_URL = 'https://61vdohhos4.execute-api.us-west-1.amazonaws.com/dev/api/v2/';
 // export const API_URL = 'http://localhost:2000/api/v2/';
 
-export const BING_LCOATION_API_URL = 'http://dev.virtualearth.net/REST/v1/Locations/';
+export const BING_LCOATION_API_URL = 'https://dev.virtualearth.net/REST/v1/Locations/';
 
 // Names for default Options for venue selections
 
