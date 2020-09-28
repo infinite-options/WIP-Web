f5255931c53299169247be35a11f2bbbdadd759e
Tejas Ghanwat <tejas.aghanwat@gmail.com>
2020-09-28 16:34:13 -0700

    Homepage layout updated

--- web/src/components/Home/index.jsx
+++ web/src/components/Home/index.jsx
@@ -1,56 +1,57 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 import { Grid } from "@material-ui/core";
 
-import SelectVenue from './selectVenue';
-import QueueTable from './queueTable';
-import BusinessHours from './businessHours';
-import DefaultWaitTime from './defaultWaitTime';
-import MaxCapacity from './maxCapacity';
-import InVenue from './inVenue';
-import InQueue from './inQueue';
+import SelectVenue from "./selectVenue";
+import QueueTable from "./queueTable";
+import BusinessHours from "./businessHours";
+import DefaultWaitTime from "./defaultWaitTime";
+import MaxCapacity from "./maxCapacity";
+import InVenue from "./inVenue";
+import InQueue from "./inQueue";
 
-import styles from './home.module.css';
+import styles from "./home.module.css";
 
 function Home() {
-    return (
-        <div className={styles.home}>
-            <SelectVenue/>
-            <Grid container className={styles.mainSection}>
-            <Grid item xs={12} md={8}>
-                <Grid container>
-                    <Grid item xs={12}>
-                        <QueueTable />
-                    </Grid>
-                    <Grid item xs={12} md={3}>
-                        <DefaultWaitTime />
-                    </Grid>
-                    <Grid item xs={12} md={3}>
-                        <MaxCapacity />
-                    </Grid>
-                    <Grid item xs={12} md={3}>
-                        <InVenue />
-                    </Grid>
-                    <Grid item xs={12} md={3}>
-                        <InQueue />
-                    </Grid>
-                </Grid>
+  return (
+    <div className={styles.home}>
+      <SelectVenue />
+      <Grid container className={styles.mainSection}>
+        <Grid className={styles.leftPanel} item xs={12} md={4}>
+          <Grid className={styles.venue} item xs={10} md={12}>
+            <Grid item xs={6} md={6}>
+              <InVenue />
             </Grid>
-            <Grid item xs={12} md={4}>
-                <BusinessHours />
+            <Grid item xs={6} md={6}>
+              <InQueue />
             </Grid>
+            <Grid item xs={6} md={6}>
+              <MaxCapacity />
             </Grid>
-        </div>
-    )
+            <Grid item xs={6} md={6}>
+              <DefaultWaitTime />
+            </Grid>
+          </Grid>
+          <BusinessHours />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Grid container>
+            <Grid item xs={12}>
+              <QueueTable />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 Home.propTypes = {
-    fetchVenues: PropTypes.func.isRequired,
-}
+  fetchVenues: PropTypes.func.isRequired,
+};
 
-const mapStateToProps = state => ({
-})
+const mapStateToProps = (state) => ({});
 
-export default connect(mapStateToProps, { })(Home);
+export default connect(mapStateToProps, {})(Home);
